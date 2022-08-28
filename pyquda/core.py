4a58fefc8e6130cb839e9cc9db1510a44527358a
SaltyChiang <SaltyChiang@users.noreply.github.com>
2022-08-28 12:15:43 +0800

    Load full gauge when smearing.

--- pyquda/core.py
+++ pyquda/core.py
@@ -152,20 +152,20 @@ class LatticePropagator(LatticeField):
 
 def smear(latt_size: List[int], gauge: LatticeGauge, nstep: int, rho: float):
     dslash = getDslash(latt_size, 0, 0, 0)
+    dslash.gauge_param.reconstruct = enum_quda.QudaReconstructType.QUDA_RECONSTRUCT_NO
     dslash.gauge_param.t_boundary = enum_quda.QudaTboundary.QUDA_PERIODIC_T
     dslash.loadGauge(gauge)
     quda.performSTOUTnStep(nstep, rho, 1)
-    dslash.gauge_param.reconstruct = enum_quda.QudaReconstructType.QUDA_RECONSTRUCT_NO
     dslash.gauge_param.type = enum_quda.QudaLinkType.QUDA_SMEARED_LINKS
     quda.saveGaugeQuda(gauge.data_ptr, dslash.gauge_param)
 
 
 def smear4(latt_size: List[int], gauge: LatticeGauge, nstep: int, rho: float):
     dslash = getDslash(latt_size, 0, 0, 0)
+    dslash.gauge_param.reconstruct = enum_quda.QudaReconstructType.QUDA_RECONSTRUCT_NO
     dslash.gauge_param.t_boundary = enum_quda.QudaTboundary.QUDA_PERIODIC_T
     dslash.loadGauge(gauge)
     quda.performOvrImpSTOUTnStep(nstep, rho, 1.0, 1)
-    dslash.gauge_param.reconstruct = enum_quda.QudaReconstructType.QUDA_RECONSTRUCT_NO
     dslash.gauge_param.type = enum_quda.QudaLinkType.QUDA_SMEARED_LINKS
     quda.saveGaugeQuda(gauge.data_ptr, dslash.gauge_param)
 
