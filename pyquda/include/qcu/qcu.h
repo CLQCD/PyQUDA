509c6d2276b4dd93e9dfa858e408cf2f8325d1ec
SaltyChiang <SaltyChiang@users.noreply.github.com>
2023-07-25 10:51:25 +0800

    Bug fix.

--- pyquda/include/qcu/qcu.h
+++ pyquda/include/qcu/qcu.h
@@ -8,7 +8,7 @@ typedef struct QcuParam_s {
   int lattice_size[4];
 } QcuParam;
 
-void dslashQcu(void *fermion_in, void *fermion_out, void *gauge, QcuParam *param);
+void dslashQcu(void *fermion_out, void *fermion_in, void *gauge, QcuParam *param);
 
 #ifdef __cplusplus
 }
