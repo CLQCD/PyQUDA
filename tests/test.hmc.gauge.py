a46c7bf9aa4773aa4ec5e17fec56dffa2a3b67a6
SaltyChiang <SaltyChiang@users.noreply.github.com>
2024-04-08 03:22:06 +0800

    Update test file for gauge HMC.

--- tests/test.hmc.gauge.py
+++ tests/test.hmc.gauge.py
@@ -5,112 +5,22 @@ import numpy as np
 from check_pyquda import test_dir
 
 from pyquda import init, setGPUID
-from pyquda.hmc_gauge import HMC
-from pyquda.field import Nc, LatticeInfo, LatticeGauge
+from pyquda.hmc import HMC
+from pyquda.action import symanzik_gauge
+from pyquda.field import LatticeInfo, LatticeGauge
 
-setGPUID(2)
+setGPUID(0)
 init(resource_path=".cache")
 beta = 6.2
 u_0 = 0.855453
 latt_info = LatticeInfo([16, 16, 16, 32], 1, 1.0)
-Lx, Ly, Lz, Lt = latt_info.size
 
+monomials = [symanzik_gauge.SymanzikGauge(latt_info, beta, u_0)]
 gauge = LatticeGauge(latt_info, None)
 
-hmc = HMC(latt_info)
+hmc = HMC(latt_info, monomials)
 hmc.loadGauge(gauge)
 hmc.loadMom(gauge)
-
-
-def loop_ndarray(path, num_paths, max_length):
-    ret = -np.ones((num_paths, max_length), "<i4")
-    for i in range(num_paths):
-        for j in range(len(path[i])):
-            ret[i, j] = path[i][j]
-    return ret
-
-
-def path_ndarray(path, num_paths, max_length):
-    ret = -np.ones((4, num_paths, max_length), "<i4")
-    for d in range(4):
-        for i in range(num_paths):
-            for j in range(len(path[d][i])):
-                ret[d, i, j] = path[d][i][j]
-    return ret
-
-
-def path_force(path, coeffs):
-    num_paths = len(path)
-    lengths = []
-    force = [[], [], [], []]
-    fcoeffs = [[], [], [], []]
-    flengths = [[], [], [], []]
-    for i in range(num_paths):
-        lengths.append(len(path[i]))
-        loop = np.array(path[i])
-        loop_dag = np.flip(7 - loop)
-        for j in range(lengths[i]):
-            if loop[j] < 4:
-                force[loop[j]].append(np.roll(loop, -j)[1:])
-                fcoeffs[loop[j]].append(-coeffs[i])
-                flengths[loop[j]].append(lengths[i] - 1)
-            else:
-                force[loop_dag[lengths[i] - 1 - j]].append(np.roll(loop_dag, j + 1 - lengths[i])[1:])
-                fcoeffs[loop_dag[lengths[i] - 1 - j]].append(-coeffs[i])
-                flengths[loop_dag[lengths[i] - 1 - j]].append(lengths[i] - 1)
-    max_length = max(lengths)
-    lengths = np.array(lengths, dtype="<i4")
-    coeffs = np.array(coeffs, "<f8")
-    path = loop_ndarray(path, num_paths, max_length)
-    assert flengths[0] == flengths[1] == flengths[2] == flengths[3], "path is not symmetric"
-    flengths = np.array(flengths[0], "<i4")
-    assert fcoeffs[0] == fcoeffs[1] == fcoeffs[2] == fcoeffs[3], "path is not symmetric"
-    fcoeffs = np.array(fcoeffs[0], "<f8")
-    num_fpaths = len(flengths)
-    force = path_ndarray(force, num_fpaths, max_length - 1)
-    return num_paths, max_length, path, lengths, coeffs, force, num_fpaths, flengths, fcoeffs
-
-
-input_path = [
-    [0, 1, 7, 6],
-    [0, 2, 7, 5],
-    [0, 3, 7, 4],
-    [1, 2, 6, 5],
-    [1, 3, 6, 4],
-    [2, 3, 5, 4],
-    [0, 0, 1, 7, 7, 6],
-    [0, 0, 2, 7, 7, 5],
-    [0, 0, 3, 7, 7, 4],
-    [1, 1, 0, 6, 6, 7],
-    [1, 1, 2, 6, 6, 5],
-    [1, 1, 3, 6, 6, 4],
-    [2, 2, 0, 5, 5, 7],
-    [2, 2, 1, 5, 5, 6],
-    [2, 2, 3, 5, 5, 4],
-    [3, 3, 0, 4, 4, 7],
-    [3, 3, 1, 4, 4, 6],
-    [3, 3, 2, 4, 4, 5],
-]
-input_coeffs = [
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    1 / 20 / u_0**2,
-    1 / 20 / u_0**2,
-    1 / 20 / u_0**2,
-    1 / 20 / u_0**2,
-    1 / 20 / u_0**2,
-    1 / 20 / u_0**2,
-    1 / 20 / u_0**2,
-    1 / 20 / u_0**2,
-    1 / 20 / u_0**2,
-    1 / 20 / u_0**2,
-    1 / 20 / u_0**2,
-    1 / 20 / u_0**2,
-]
 
 # input_path = [
 #     [0, 1, 7, 6],
@@ -128,12 +38,6 @@ input_coeffs = [
 #     -1,
 #     -1,
 # ]
-
-num_paths, max_length, path, lengths, coeffs, force, num_fpaths, flengths, fcoeffs = path_force(
-    input_path, input_coeffs
-)
-coeffs *= beta / Nc
-fcoeffs *= beta / Nc
 
 rho_ = 0.2539785108410595
 theta_ = -0.03230286765269967
@@ -153,26 +57,26 @@ for i in range(2000):
     hmc.gaussMom(i)
 
     kinetic = hmc.actionMom()
-    potential = hmc.actionGauge(path, lengths, coeffs, num_paths, max_length)
+    potential = hmc.actionGauge()
     energy = kinetic + potential
 
     for step in range(steps):
-        hmc.computeGaugeForce(vartheta_ * dt, force, flengths, fcoeffs, num_fpaths, max_length - 1)
-        hmc.updateGaugeField(rho_ * dt)
-        hmc.computeGaugeForce(lambda_ * dt, force, flengths, fcoeffs, num_fpaths, max_length - 1)
-        hmc.updateGaugeField(theta_ * dt)
-        hmc.computeGaugeForce((0.5 - (lambda_ + vartheta_)) * dt, force, flengths, fcoeffs, num_fpaths, max_length - 1)
-        hmc.updateGaugeField((1.0 - 2 * (theta_ + rho_)) * dt)
-        hmc.computeGaugeForce((0.5 - (lambda_ + vartheta_)) * dt, force, flengths, fcoeffs, num_fpaths, max_length - 1)
-        hmc.updateGaugeField(theta_ * dt)
-        hmc.computeGaugeForce(lambda_ * dt, force, flengths, fcoeffs, num_fpaths, max_length - 1)
-        hmc.updateGaugeField(rho_ * dt)
-        hmc.computeGaugeForce(vartheta_ * dt, force, flengths, fcoeffs, num_fpaths, max_length - 1)
+        hmc.updateMom(vartheta_ * dt)
+        hmc.updateGauge(rho_ * dt)
+        hmc.updateMom(lambda_ * dt)
+        hmc.updateGauge(theta_ * dt)
+        hmc.updateMom((0.5 - (lambda_ + vartheta_)) * dt)
+        hmc.updateGauge((1.0 - 2 * (theta_ + rho_)) * dt)
+        hmc.updateMom((0.5 - (lambda_ + vartheta_)) * dt)
+        hmc.updateGauge(theta_ * dt)
+        hmc.updateMom(lambda_ * dt)
+        hmc.updateGauge(rho_ * dt)
+        hmc.updateMom(vartheta_ * dt)
 
-    hmc.reunitGaugeField(1e-15)
+    hmc.reunitGauge(1e-15)
 
     kinetic1 = hmc.actionMom()
-    potential1 = hmc.actionGauge(path, lengths, coeffs, num_paths, max_length)
+    potential1 = hmc.actionGauge()
     energy1 = kinetic1 + potential1
 
     accept = np.random.rand() < np.exp(energy - energy1)
