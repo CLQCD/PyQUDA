75e39aba7c9c14260749b3df9f5bcd80afeba171
SaltyChiang <SaltyChiang@users.noreply.github.com>
2024-05-19 21:41:02 +0800

    Update `test.clover.distance.py`.

--- tests/test.clover.distance.py
+++ tests/test.clover.distance.py
@@ -1,39 +1,28 @@
-import os
-import sys
 import cupy as cp
 
-test_dir = os.path.dirname(os.path.abspath(__file__))
-# sys.path.insert(0, os.path.join(test_dir, ".."))
+from check_pyquda import weak_field
+
 from pyquda import core, init
 from pyquda.utils import io
-
-os.environ["QUDA_RESOURCE_PATH"] = ".cache"
-init()
-
-latt_size = [4, 4, 4, 8]
-Lx, Ly, Lz, Lt = latt_size
-Vol = Lx * Ly * Lz * Lt
+from pyquda.dirac import setPrecision
 
 xi_0, nu = 2.464, 0.95
 kappa = 0.115
+mass = 1 / (2 * kappa) - 4
 coeff = 1.17
 coeff_r, coeff_t = 0.91, 1.07
-alpha0 = 0.4
-t0 = 0
 
-mass = 1 / (2 * kappa) - 4
+init([1, 1, 1, 1], [4, 4, 4, 8], -1, xi_0 / nu, resource_path=".cache")
 
-from pyquda.dirac import general
-
-dslash = core.getDslash(latt_size, mass, 1e-9, 1000, xi_0, nu, coeff_t, coeff_r, multigrid=False)
-general.cuda_prec_sloppy = 8
-dslash.invert_param.distance_pc_alpha0 = alpha0
-dslash.invert_param.distance_pc_t0 = t0
-gauge = io.readQIOGauge(os.path.join(test_dir, "weak_field.lime"))
-
+setPrecision(sloppy=8)
+dslash = core.getDefaultDirac(mass, 1e-12, 1000, xi_0, coeff_t, coeff_r)
+gauge = io.readQIOGauge(weak_field)
 
 dslash.loadGauge(gauge)
 
+alpha0, t0 = 0.4, 0
+dslash.invert_param.distance_pc_alpha0 = alpha0
+dslash.invert_param.distance_pc_t0 = t0
 propagator = core.invert(dslash, "point", [0, 0, 0, t0])
 
 dslash.destroy()
