a8ab3a108777f4dc86f567324b95158b7b697d0c
SaltyChiang <SaltyChiang@users.noreply.github.com>
2024-07-21 16:38:33 +0800

    Update `MomentumPhase`.

--- pyquda/utils/phase.py
+++ pyquda/utils/phase.py
@@ -1,5 +1,5 @@
 from math import pi
-from typing import NamedTuple, Sequence
+from typing import Sequence
 
 import numpy
 
@@ -40,122 +40,88 @@ def getMomDict(mom2_max, mom2_min=0):
     return mom_dict
 
 
-class ThreeMomentumMode(NamedTuple):
-    x: int
-    y: int
-    z: int
-
-
-class FourMomentumMode(NamedTuple):
-    x: int
-    y: int
-    z: int
-    t: int
-
-
 class MomentumPhase:
     def __init__(self, latt_info: LatticeInfo) -> None:
         from .. import getCUDABackend
 
         self.latt_info = latt_info
         gx, gy, gz, gt = latt_info.grid_coord
-        GLx, GLy, GLz, GLt = latt_info.global_size
         Lx, Ly, Lz, Lt = latt_info.size
 
-        x = numpy.zeros((2, Lt, Lz, Ly, Lx // 2), "<c16")
-        y = numpy.zeros((2, Lt, Lz, Ly, Lx // 2), "<c16")
-        z = numpy.zeros((2, Lt, Lz, Ly, Lx // 2), "<c16")
-        t = numpy.zeros((2, Lt, Lz, Ly, Lx // 2), "<c16")
-
-        xx = 2j * pi / GLx * numpy.arange(gx * Lx, (gx + 1) * Lx)
+        x = numpy.zeros((4, 2, Lt, Lz, Ly, Lx // 2), "<c16")
+        xx = numpy.arange(gx * Lx, (gx + 1) * Lx)
         for it in range(Lt):
             for iz in range(Lz):
                 for iy in range(Ly):
                     ieo = (it + iz + iy) % 2
                     if ieo == 0:
-                        x[0, it, iz, iy] = xx[0::2]
-                        x[1, it, iz, iy] = xx[1::2]
+                        x[0, 0, it, iz, iy] = xx[0::2]
+                        x[0, 1, it, iz, iy] = xx[1::2]
                     else:
-                        x[1, it, iz, iy] = xx[0::2]
-                        x[0, it, iz, iy] = xx[1::2]
-        y[:] = 2j * pi / GLy * numpy.arange(gy * Ly, (gy + 1) * Ly).reshape(1, 1, 1, Ly, 1)
-        z[:] = 2j * pi / GLz * numpy.arange(gz * Lz, (gz + 1) * Lz).reshape(1, 1, Lz, 1, 1)
-        t[:] = 2j * pi / GLt * numpy.arange(gt * Lt, (gt + 1) * Lt).reshape(1, Lt, 1, 1, 1)
+                        x[0, 1, it, iz, iy] = xx[0::2]
+                        x[0, 0, it, iz, iy] = xx[1::2]
+        x[1] = numpy.arange(gy * Ly, (gy + 1) * Ly).reshape(1, 1, 1, Ly, 1)
+        x[2] = numpy.arange(gz * Lz, (gz + 1) * Lz).reshape(1, 1, Lz, 1, 1)
+        x[3] = numpy.arange(gt * Lt, (gt + 1) * Lt).reshape(1, Lt, 1, 1, 1)
 
         backend = getCUDABackend()
         if backend == "numpy":
             self.x = x
-            self.y = y
-            self.z = z
-            self.t = t
         elif backend == "cupy":
             import cupy
 
             self.x = cupy.asarray(x)
-            self.y = cupy.asarray(y)
-            self.z = cupy.asarray(z)
-            self.t = cupy.asarray(t)
         elif backend == "torch":
             import torch
 
             self.x = torch.as_tensor(x)
-            self.y = torch.as_tensor(y)
-            self.z = torch.as_tensor(z)
-            self.t = torch.as_tensor(t)
 
-    def getPhase(self, mom: Sequence[int]):
+    def getPhase(self, mom_mode: Sequence[int]):
         from .. import getCUDABackend
 
+        x = self.x
+        global_size = self.latt_info.global_size
+
+        if len(mom_mode) == 3:
+            ip = [2j * pi * mom_mode[i] / global_size[i] for i in range(3)]
+            ipx = ip[0] * x[0] + ip[1] * x[1] + ip[2] * x[2]
+        elif len(mom_mode) == 4:
+            ip = [2j * pi * mom_mode[i] / global_size[i] for i in range(4)]
+            ipx = ip[0] * x[0] + ip[1] * x[1] + ip[2] * x[2] + ip[3] * x[3]
+        else:
+            from .. import getLogger
+
+            getLogger().critical(f"mom should be a sequence of int with length 3 or 4, but get {mom_mode}", ValueError)
+
         backend = getCUDABackend()
-        if not isinstance(mom, ThreeMomentumMode) or not isinstance(mom, FourMomentumMode):
-            if len(mom) == 3:
-                mom = ThreeMomentumMode(*mom)
-            elif len(mom) == 4:
-                mom = FourMomentumMode(*mom)
-            else:
-                from .. import getLogger
+        if backend == "numpy":
+            return numpy.exp(ipx)
+        elif backend == "cupy":
+            import cupy
 
-                getLogger().critical(f"mom should be a sequence of int with length 3 or 4, but get {mom}", ValueError)
-        if isinstance(mom, ThreeMomentumMode):
-            if backend == "numpy":
-                return numpy.exp(mom.x * self.x + mom.y * self.y + mom.z * self.z)
-            elif backend == "cupy":
-                import cupy
+            return cupy.exp(ipx)
+        elif backend == "torch":
+            import torch
 
-                return cupy.exp(mom.x * self.x + mom.y * self.y + mom.z * self.z)
-            elif backend == "torch":
-                import torch
+            return torch.exp(ipx)
 
-                return torch.exp(mom.x * self.x + mom.y * self.y + mom.z * self.z)
-        elif isinstance(mom, FourMomentumMode):
-            if backend == "numpy":
-                return numpy.exp(mom.x * self.x + mom.y * self.y + mom.z * self.z + mom.t * self.t)
-            elif backend == "cupy":
-                import cupy
-
-                return cupy.exp(mom.x * self.x + mom.y * self.y + mom.z * self.z + mom.t * self.t)
-            elif backend == "torch":
-                import torch
-
-                return torch.exp(mom.x * self.x + mom.y * self.y + mom.z * self.z + mom.t * self.t)
-
-    def getPhases(self, mom_list: Sequence[Sequence[int]]):
+    def getPhases(self, mom_mode_list: Sequence[Sequence[int]]):
         from .. import getCUDABackend
 
         Lx, Ly, Lz, Lt = self.latt_info.size
 
         backend = getCUDABackend()
         if backend == "numpy":
-            phases = numpy.zeros((len(mom_list), 2, Lt, Lz, Ly, Lx // 2), "<c16")
+            phases = numpy.zeros((len(mom_mode_list), 2, Lt, Lz, Ly, Lx // 2), "<c16")
         elif backend == "cupy":
             import cupy
 
-            phases = cupy.zeros((len(mom_list), 2, Lt, Lz, Ly, Lx // 2), "<c16")
+            phases = cupy.zeros((len(mom_mode_list), 2, Lt, Lz, Ly, Lx // 2), "<c16")
         elif backend == "torch":
             import torch
 
-            phases = torch.zeros((len(mom_list), 2, Lt, Lz, Ly, Lx // 2), dtype=torch.complex128)
-        for idx, mom in enumerate(mom_list):
+            phases = torch.zeros((len(mom_mode_list), 2, Lt, Lz, Ly, Lx // 2), dtype=torch.complex128)
+        for idx, mom in enumerate(mom_mode_list):
             phases[idx] = self.getPhase(mom)
 
         return phases
@@ -173,11 +139,10 @@ class GridPhase:
         Lx, Ly, Lz, Lt = self.latt_info.size
         Sx, Sy, Sz, St = self.stride
         x, y, z, t = t_srce
+        sx, sy, sz, st = (x + gx * Lx) % Sx, (y + gy * Ly) % Sy, (z + gz * Lz) % Sz, (t + gt * Lt) % St
         phase = numpy.zeros((Lt, Lz, Ly, Lx), "<i4")
-        if (t + gt * Lt) % St < Lt and (z + gz * Lz) % Sz < Lz and (y + gy * Ly) % Sy < Ly and (x + gx * Lx) % Sx < Lx:
-            phase[
-                (t + gt * Lt) % St :: St, (z + gz * Lz) % Sz :: Sz, (y + gy * Ly) % Sy :: Sy, (x + gx * Lx) % Sx :: Sx
-            ] = 1
+        if sx < Lx and sy < Ly and sz < Lz and st < Lt:
+            phase[st::St, sz::Sz, sy::Sy, sx::Sx] = 1
         phase = cb2(phase, [0, 1, 2, 3])
 
         backend = getCUDABackend()
