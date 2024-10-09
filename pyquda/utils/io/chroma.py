fc2409a96e331c960ac5c99af4289a29bf29e5d9
SaltyChiang <SaltyChiang@users.noreply.github.com>
2024-10-09 22:21:01 +0800

    Calculate checksum for MILC gauge and Chroma gauge/propagator file.

--- pyquda/utils/io/chroma.py
+++ pyquda/utils/io/chroma.py
@@ -10,6 +10,31 @@ Nd, Ns, Nc = 4, 4, 3
 _precision_map = {"D": 8, "F": 4, "S": 4}
 
 
+def checksum(latt_size: List[int], data):
+    import zlib
+    import numpy
+    from mpi4py import MPI
+    from ... import getMPIComm, getGridCoord
+
+    gx, gy, gz, gt = getGridCoord()
+    Lx, Ly, Lz, Lt = getSublatticeSize(latt_size)
+    gLx, gLy, gLz, gLt = gx * Lx, gy * Ly, gz * Lz, gt * Lt
+    GLx, GLy, GLz, GLt = latt_size
+    work = numpy.empty((Lt * Lz * Ly * Lx), "<u4")
+    for i in range(Lt * Lz * Ly * Lx):
+        work[i] = zlib.crc32(data[i])
+    rank = (
+        numpy.arange(GLt * GLz * GLy * GLx, dtype="<u4")
+        .reshape(GLt, GLz, GLy, GLx)[gLt : gLt + Lt, gLz : gLz + Lz, gLy : gLy + Ly, gLx : gLx + Lx]
+        .reshape(-1)
+    )
+    rank29 = rank % 29
+    rank31 = rank % 31
+    sum29 = getMPIComm().allreduce(numpy.bitwise_xor.reduce(work << rank29 | work >> (32 - rank29)).item(), MPI.BXOR)
+    sum31 = getMPIComm().allreduce(numpy.bitwise_xor.reduce(work << rank31 | work >> (32 - rank31)).item(), MPI.BXOR)
+    return sum29, sum31
+
+
 def readQIOGauge(filename: str):
     filename = path.expanduser(path.expandvars(filename))
     with open(filename, "rb") as f:
@@ -31,6 +56,10 @@ def readQIOGauge(filename: str):
         scidac_private_record_xml = ET.ElementTree(
             ET.fromstring(f.read(meta["scidac-private-record-xml"][1]).strip(b"\x00").decode("utf-8"))
         )
+        f.seek(meta["scidac-checksum"][0])
+        scidac_checksum_xml = ET.ElementTree(
+            ET.fromstring(f.read(meta["scidac-checksum"][1]).strip(b"\x00").decode("utf-8"))
+        )
         offset = meta["ildg-binary-data"][0]
     precision = _precision_map[scidac_private_record_xml.find("precision").text]
     assert int(scidac_private_record_xml.find("colors").text) == Nc
@@ -46,6 +75,10 @@ def readQIOGauge(filename: str):
     dtype = f">c{2*precision}"
 
     gauge = readMPIFile(filename, dtype, offset, (Lt, Lz, Ly, Lx, Nd, Nc, Nc), (3, 2, 1, 0))
+    assert checksum(latt_size, gauge.reshape(Lt * Lz * Ly * Lx, Nd * Nc * Nc)) == (
+        int(scidac_checksum_xml.find("suma").text, 16),
+        int(scidac_checksum_xml.find("sumb").text, 16),
+    ), f"Bad checksum for {filename}"
     gauge = gauge.transpose(4, 0, 1, 2, 3, 5, 6).astype("<c16")
     return latt_size, gauge
 
@@ -81,6 +114,10 @@ def readQIOPropagator(filename: str):
         scidac_private_record_xml = ET.ElementTree(
             ET.fromstring(f.read(meta["scidac-private-record-xml"][1]).strip(b"\x00").decode("utf-8"))
         )
+        f.seek(meta["scidac-checksum"][0])
+        scidac_checksum_xml = ET.ElementTree(
+            ET.fromstring(f.read(meta["scidac-checksum"][1]).strip(b"\x00").decode("utf-8"))
+        )
         offset = meta["scidac-binary-data"][0]
     precision = _precision_map[scidac_private_record_xml.find("precision").text]
     assert int(scidac_private_record_xml.find("colors").text) == Nc
@@ -103,8 +140,16 @@ def readQIOPropagator(filename: str):
 
     if not staggered:
         propagator = readMPIFile(filename, dtype, offset, (Lt, Lz, Ly, Lx, Ns, Ns, Nc, Nc), (3, 2, 1, 0))
+        assert checksum(latt_size, propagator.reshape(Lt * Lz * Ly * Lx, Ns * Ns * Nc * Nc)) == (
+            int(scidac_checksum_xml.find("suma").text, 16),
+            int(scidac_checksum_xml.find("sumb").text, 16),
+        ), f"Bad checksum for {filename}"
         propagator = propagator.astype("<c16")
     else:
         propagator = readMPIFile(filename, dtype, offset, (Lt, Lz, Ly, Lx, Nc, Nc), (3, 2, 1, 0))
+        assert checksum(latt_size, propagator.reshape(Lt * Lz * Ly * Lx, Nc * Nc)) == (
+            int(scidac_checksum_xml.find("suma").text, 16),
+            int(scidac_checksum_xml.find("sumb").text, 16),
+        ), f"Bad checksum for {filename}"
         propagator = propagator.astype("<c16")
     return latt_size, staggered, propagator
