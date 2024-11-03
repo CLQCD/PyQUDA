d9267e35fe9abf4a3275c3773f42543d1180e1c8
SaltyChiang <SaltyChiang@users.noreply.github.com>
2024-11-04 03:34:49 +0800

    Update `pyproject.toml`.

---
+++ setup.py
@@ -0,0 +1,12 @@
+import os
+import sys
+from setuptools import setup
+
+if "egg_info" in sys.argv or "sdist" in sys.argv:
+    describe = os.popen("git describe --tags", "r").read()
+    if describe != "":
+        tag, post, hash = describe.strip().split("-")
+        with open(os.path.join(os.path.dirname(__file__), "pyquda_utils", "_version.py"), "w") as f:
+            f.write(f'__version__ = "{tag[1:]}.post{post}+{hash}"\n')
+
+setup()
