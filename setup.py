b513491aaf49ab8d1d9453ff19928028706a4277
SaltyChiang <SaltyChiang@users.noreply.github.com>
2024-11-06 02:44:36 +0800

    Fix package version of PyQUDA-Utils.

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ import sys
 from setuptools import setup
 
 if "egg_info" in sys.argv or "sdist" in sys.argv:
-    describe = os.popen("git describe --tags", "r").read()
+    describe = os.popen("git describe --tags", "r").read().strip()
     if describe != "":
-        tag, post, hash = describe.strip().split("-")
+        if "-" in describe:
+            tag, post, _ = describe.split("-")
+        else:
+            tag, post = describe, 0
         with open(os.path.join(os.path.dirname(__file__), "pyquda_utils", "_version.py"), "w") as f:
             f.write(f'__version__ = "{tag[1:]}.dev{post}"\n')
 
