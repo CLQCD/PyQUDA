a8c7a67c479ea87462139031239e9dc3ef02b809
SaltyChiang <SaltyChiang@users.noreply.github.com>
2025-07-21 15:45:02 +0800

    Add support for device FFT.

--- pyquda_utils/fft.py
+++ pyquda_utils/fft.py
@@ -1,24 +1,67 @@
-from copy import copy
 from math import prod
-from numbers import Integral
 from time import perf_counter
-from typing import List, Tuple
+from typing import List, Sequence, Union
 
 import numpy
 from numpy.typing import NDArray
 from mpi4py import MPI
 from mpi4py.util import dtlib
 
-from pyquda_comm import (
-    getMPIComm,
-    getMPISize,
-    getMPIRank,
-    getGridSize,
-    getGridCoord,
-    getCoordFromRank,
-    getRankFromCoord,
-)
+from pyquda_comm import getMPIComm, getMPISize, getMPIRank, getGridSize, getGridCoord, getCoordFromRank
+from pyquda_comm.array import BackendType, arrayHostCopy, arrayDevice
 from pyquda_comm.field import LatticeInfo, LatticePropagator
+
+
+def arrayFFTN(data, axes: Union[int, Sequence[int]], backend: BackendType) -> NDArray:
+    if isinstance(axes, int):
+        axis = axes
+        if backend == "numpy":
+            return numpy.fft.fft(data, axis=axis)
+        elif backend == "cupy":
+            import cupy
+
+            return cupy.fft.fft(data, axis=axis)
+        elif backend == "torch":
+            import torch
+
+            return torch.fft.fft(data, dim=axis, norm="backward")
+    else:
+        if backend == "numpy":
+            return numpy.fft.fftn(data, axes=axes)
+        elif backend == "cupy":
+            import cupy
+
+            return cupy.fft.fftn(data, axes=axes)
+        elif backend == "torch":
+            import torch
+
+            return torch.fft.fftn(data, dim=axes, norm="backward")
+
+
+def arrayIFFTN(data, axes: Union[int, Sequence[int]], backend: BackendType) -> NDArray:
+    if isinstance(axes, int):
+        axis = axes
+        if backend == "numpy":
+            return numpy.fft.ifft(data, axis=axis)
+        elif backend == "cupy":
+            import cupy
+
+            return cupy.fft.ifft(data, axis=axis)
+        elif backend == "torch":
+            import torch
+
+            return torch.fft.ifft(data, dim=axis, norm="backward")
+    else:
+        if backend == "numpy":
+            return numpy.fft.ifftn(data, axes=axes)
+        elif backend == "cupy":
+            import cupy
+
+            return cupy.fft.ifftn(data, axes=axes)
+        elif backend == "torch":
+            import torch
+
+            return torch.fft.ifftn(data, dim=axes, norm="backward")
 
 
 def loadBalanceSubsize(GL: int, size: int, rank: int):
@@ -29,9 +72,7 @@ def loadBalanceStart(GL: int, size: int, rank: int):
     return GL // size * rank + (rank if rank < GL % size else GL % size)
 
 
-def transform(
-    latt_info: LatticeInfo, field_shape: List[int], recvbuf: NDArray, recvdim: int, sendbuf: NDArray, senddim: int
-):
+def transform(latt_info: LatticeInfo, field_shape: List[int], recvdim: int, senddim: int, buf: NDArray):
     latt_size = latt_info.global_size
     size = getMPISize()
     rank = getMPIRank()
@@ -65,7 +106,8 @@ def transform(
             else MPI.COMPLEX16
         )
 
-    s = perf_counter()
+    sendbuf = buf.reshape(send_sizes[::-1] + field_shape)
+    recvbuf = numpy.empty(recv_sizes[::-1] + field_shape, buf.dtype)
     for i in range(size):
         sendtypes[i].Commit()
         recvtypes[i].Commit()
@@ -73,7 +115,8 @@ def transform(
     for i in range(size):
         sendtypes[i].Free()
         recvtypes[i].Free()
-    print(f"Alltoallw time: {perf_counter() - s:.6f} seconds")
+
+    return recvbuf
 
 
 def redistribute(latt_info: LatticeInfo, field_shape: List[int], dim: int, buf: NDArray):
@@ -132,53 +175,139 @@ def redistribute(latt_info: LatticeInfo, field_shape: List[int], dim: int, buf: 
 
     sendbuf = buf.reshape(send_sizes[::-1] + field_shape)
     recvbuf = numpy.empty_like(buf, shape=recv_sizes[::-1] + field_shape)
-    s = perf_counter()
     for i in range(size):
         if sendtypes[i] != MPI.COMPLEX16:
             sendtypes[i].Commit()
         if recvtypes[i] != MPI.COMPLEX16:
             recvtypes[i].Commit()
-    getMPIComm().Alltoallw(
-        (
-            sendbuf,
-            [0 if sendtype == MPI.COMPLEX16 else 1 for sendtype in sendtypes],
-            [0 for sendtype in sendtypes],
-            sendtypes,
-        ),
-        (
-            recvbuf,
-            [0 if recvtype == MPI.COMPLEX16 else 1 for recvtype in recvtypes],
-            [0 for recvtype in recvtypes],
-            recvtypes,
-        ),
-    )
+    sendcounts = [0 if sendtype == MPI.COMPLEX16 else 1 for sendtype in sendtypes]
+    senddispls = [0 for _ in sendtypes]
+    recvcounts = [0 if recvtype == MPI.COMPLEX16 else 1 for recvtype in recvtypes]
+    recvdispls = [0 for _ in recvtypes]
+    getMPIComm().Alltoallw((sendbuf, sendcounts, senddispls, sendtypes), (recvbuf, recvcounts, recvdispls, recvtypes))
     for i in range(size):
         if sendtypes[i] != MPI.COMPLEX16:
             sendtypes[i].Free()
         if recvtypes[i] != MPI.COMPLEX16:
             recvtypes[i].Free()
-    print(f"Alltoallw time: {perf_counter() - s:.6f} seconds")
 
     return recvbuf
 
 
-def fft(field: LatticePropagator):
-    from pyfftw.interfaces import numpy_fft
+def redistribute_reverse(latt_info: LatticeInfo, field_shape: List[int], dim: int, buf: NDArray):
+    size = getMPISize()
+    rank = getMPIRank()
+    GLt = latt_info.global_size[dim]
+    Lt = latt_info.size[dim]
 
+    recvtypes = []
+    sendtypes = []
+    recv_sizes = [L for L in latt_info.size]
+    recv_subsizes = [L for L in latt_info.size]
+    send_sizes = [GL for GL in latt_info.global_size]
+    send_sizes[dim] = loadBalanceSubsize(GLt, size, rank)
+    send_subsizes = [L for L in latt_info.size]
+
+    start_r = loadBalanceStart(GLt, size, rank)
+    subsize_r = loadBalanceSubsize(GLt, size, rank)
+    gLt_r = getCoordFromRank(rank)[dim] * Lt
+    for i in range(size):
+        start_i = loadBalanceStart(GLt, size, i)
+        subsize_i = loadBalanceSubsize(GLt, size, i)
+        gLt_i = getCoordFromRank(i)[dim] * Lt
+
+        start = max(start_i, gLt_r)
+        stop = min(start_i + subsize_i, gLt_r + Lt)
+        if start >= stop:
+            recvtypes.append(MPI.COMPLEX16)
+        else:
+            recv_subsizes[dim] = stop - start
+            recv_starts = [0 for _ in latt_info.size]
+            recv_starts[dim] = start - gLt_r
+            recvtypes.append(
+                MPI.COMPLEX16.Create_subarray(
+                    recv_sizes[::-1] + field_shape,
+                    recv_subsizes[::-1] + field_shape,
+                    recv_starts[::-1] + [0 for _ in field_shape],
+                )
+            )
+
+        start = max(start_r, gLt_i)
+        stop = min(start_r + subsize_r, gLt_i + Lt)
+        if start >= stop:
+            sendtypes.append(MPI.COMPLEX16)
+        else:
+            send_subsizes[dim] = stop - start
+            send_starts = [g * L for g, L in zip(getCoordFromRank(i), latt_info.size)]
+            send_starts[dim] = start - start_r
+            sendtypes.append(
+                MPI.COMPLEX16.Create_subarray(
+                    send_sizes[::-1] + field_shape,
+                    send_subsizes[::-1] + field_shape,
+                    send_starts[::-1] + [0 for _ in field_shape],
+                )
+            )
+
+    sendbuf = buf.reshape(send_sizes[::-1] + field_shape)
+    recvbuf = numpy.empty_like(buf, shape=recv_sizes[::-1] + field_shape)
+    for i in range(size):
+        if sendtypes[i] != MPI.COMPLEX16:
+            sendtypes[i].Commit()
+        if recvtypes[i] != MPI.COMPLEX16:
+            recvtypes[i].Commit()
+    sendcounts = [0 if sendtype == MPI.COMPLEX16 else 1 for sendtype in sendtypes]
+    senddispls = [0 for _ in sendtypes]
+    recvcounts = [0 if recvtype == MPI.COMPLEX16 else 1 for recvtype in recvtypes]
+    recvdispls = [0 for _ in recvtypes]
+    getMPIComm().Alltoallw((sendbuf, sendcounts, senddispls, sendtypes), (recvbuf, recvcounts, recvdispls, recvtypes))
+    for i in range(size):
+        if sendtypes[i] != MPI.COMPLEX16:
+            sendtypes[i].Free()
+        if recvtypes[i] != MPI.COMPLEX16:
+            recvtypes[i].Free()
+
+    return recvbuf
+
+
+def fft(field: LatticePropagator, fft3d: bool):
+    backend = field.location
     latt_info = field.latt_info
     field_shape = field.field_shape
-    s = perf_counter()
+    Nd = latt_info.Nd
     buf = field.lexico()
-    print(f"Lexico: {perf_counter() - s} secs")
-    print(buf.shape)
-    buf = redistribute(latt_info, field_shape, 3, buf)
-    s = perf_counter()
-    buf = numpy_fft.fftn(buf, axes=[4, 5, 6, 7])
-    print(f"FFTW time: {perf_counter() - s:.6f} seconds")
-    print(buf.shape)
+    if fft3d:
+        buf = redistribute(latt_info, field_shape, Nd - 1, buf)
+        buf = arrayHostCopy(arrayFFTN(arrayDevice(buf, backend), (1, 2, 3), backend), backend)
+        buf = redistribute_reverse(latt_info, field_shape, Nd - 1, buf)
+    else:
+        buf = redistribute(latt_info, field_shape, Nd - 1, buf)
+        buf = arrayHostCopy(arrayFFTN(arrayDevice(buf, backend), (1, 2, 3), backend), backend)
+        buf = transform(latt_info, field_shape, Nd - 2, Nd - 1, buf)
+        buf = arrayHostCopy(arrayFFTN(arrayDevice(buf, backend), 0, backend), backend)
+        buf = redistribute_reverse(latt_info, field_shape, Nd - 2, buf)
+    return LatticePropagator(latt_info, arrayDevice(latt_info.evenodd(buf, False), backend))
 
 
-def fft4(field):
+def ifft(field: LatticePropagator, fft3d: bool):
+    backend = field.location
+    latt_info = field.latt_info
+    field_shape = field.field_shape
+    Nd = latt_info.Nd
+    buf = field.lexico()
+    if fft3d:
+        buf = redistribute(latt_info, field_shape, Nd - 1, buf)
+        buf = arrayHostCopy(arrayIFFTN(arrayDevice(buf, backend), (1, 2, 3), backend), backend)
+        buf = redistribute_reverse(latt_info, field_shape, Nd - 1, buf)
+    else:
+        buf = redistribute(latt_info, field_shape, Nd - 1, buf)
+        buf = arrayHostCopy(arrayIFFTN(arrayDevice(buf, backend), (1, 2, 3), backend), backend)
+        buf = transform(latt_info, field_shape, Nd - 2, Nd - 1, buf)
+        buf = arrayHostCopy(arrayIFFTN(arrayDevice(buf, backend), 0, backend), backend)
+        buf = redistribute_reverse(latt_info, field_shape, Nd - 2, buf)
+    return LatticePropagator(latt_info, arrayDevice(latt_info.evenodd(buf, False), backend))
+
+
+def fft4(field: LatticePropagator):
     buf: NDArray = field.lexico()
 
     size = getMPISize()
@@ -239,6 +368,7 @@ def fft4(field):
 
     from pyfftw.interfaces import numpy_fft
 
+    print(recvbuf.shape)
     s = perf_counter()
     sendbuf[:] = numpy_fft.fftn(recvbuf, axes=list(range(0, Nd)))
     print(f"FFTW time: {perf_counter() - s:.6f} seconds")
@@ -250,25 +380,26 @@ def fft4(field):
         gk += g[d] * k[Nd - 1 - d] / G[d] / L[d]
     sendbuf *= numpy.exp(-2j * numpy.pi * gk).reshape(*L[::-1].tolist(), *numpy.ones_like(F).tolist())
     print(f"Phase time: {perf_counter() - s:.6f} seconds")
+    rank = getMPIRank()
 
     s = perf_counter()
-    for d in range(Nd):
-        buf_hat = numpy.exp(-2j * numpy.pi * (g * g / G)[d]) * sendbuf
-        for i in range(1, grid_size[d]):
-            grid_coord[d] = (grid_coord[d] - i) % grid_size[d]
-            dest = getRankFromCoord(grid_coord)
-            grid_coord[d] = (grid_coord[d] + 2 * i) % grid_size[d]
-            source = getRankFromCoord(grid_coord)
-            grid_coord[d] = (grid_coord[d] - i) % grid_size[d]
-            getMPIComm().Sendrecv(sendbuf, dest, i, recvbuf, source, i)
-            buf_hat += numpy.exp(-2j * numpy.pi * (g * gp[source] / G)[d]) * recvbuf
-        sendbuf = buf_hat
-    # buf_hat = numpy.exp(-2j * numpy.pi * numpy.sum(g * g / G)) * sendbuf
-    # for i in range(1, size):
-    #     dest = (rank - i) % size
-    #     source = (rank + i) % size
-    #     getMPIComm().Sendrecv(sendbuf, dest, i, recvbuf, source, i)
-    #     buf_hat += numpy.exp(-2j * numpy.pi * numpy.sum(g * gp[source] / G)) * recvbuf
+    # for d in range(Nd):
+    #     buf_hat = numpy.exp(-2j * numpy.pi * (g * g / G)[d]) * sendbuf
+    #     for i in range(1, grid_size[d]):
+    #         grid_coord[d] = (grid_coord[d] - i) % grid_size[d]
+    #         dest = getRankFromCoord(grid_coord)
+    #         grid_coord[d] = (grid_coord[d] + 2 * i) % grid_size[d]
+    #         source = getRankFromCoord(grid_coord)
+    #         grid_coord[d] = (grid_coord[d] - i) % grid_size[d]
+    #         getMPIComm().Sendrecv(sendbuf, dest, i, recvbuf, source, i)
+    #         buf_hat += numpy.exp(-2j * numpy.pi * (g * gp[source] / G)[d]) * recvbuf
+    #     sendbuf = buf_hat
+    buf_hat = numpy.exp(-2j * numpy.pi * numpy.sum(g * g / G)) * sendbuf
+    for i in range(1, size):
+        dest = (rank - i) % size
+        source = (rank + i) % size
+        getMPIComm().Sendrecv(sendbuf, dest, i, recvbuf, source, i)
+        buf_hat += numpy.exp(-2j * numpy.pi * numpy.sum(g * gp[source] / G)) * recvbuf
     print(f"Sendrecv time: {perf_counter() - s:.6f} seconds")
 
     return buf_hat
---
+++ pyquda_utils/phase_v2.py
@@ -0,0 +1,72 @@
+from math import pi
+from typing import Sequence
+
+import numpy
+
+from pyquda_comm.array import arrayDevice, arrayExp
+from pyquda_comm.field import LatticeInt, MultiLatticeInt, LatticeComplex, MultiLatticeComplex
+from .core import getCUDABackend, LatticeInfo
+
+
+class LocationPhase:
+    def __init__(self, latt_info: LatticeInfo) -> None:
+        self.latt_info = latt_info
+        self.x = latt_info.evenodd(numpy.indices(latt_info.size[::-1], "<i4"), True)
+        for i in range(latt_info.Nd):
+            self.x[i] += latt_info.grid_coord[i] * latt_info.size[i]
+
+    def getPhase(self):
+        return MultiLatticeInt(self.latt_info, self.latt_info.Nd, arrayDevice(self.x, getCUDABackend()))
+
+
+class DistancePhase:
+    def __init__(self, latt_info: LatticeInfo) -> None:
+        self.latt_info = latt_info
+        self.x = LocationPhase(latt_info).x
+
+    def getPhase(self, x0: Sequence[int]):
+        phase = MultiLatticeInt(self.latt_info, self.latt_info.Nd)
+        for i in range(self.latt_info.Nd):
+            GL = self.latt_info.global_size[i]
+            phase[i].data[:] = arrayDevice((self.x[i] - x0[i] + GL // 2) % GL - GL // 2, getCUDABackend())
+        return phase
+
+
+class MomentumPhase:
+    def __init__(self, latt_info: LatticeInfo) -> None:
+        self.latt_info = latt_info
+        self.x = LocationPhase(latt_info).x
+
+    def getPhase(self, mom: Sequence[int], x0: Sequence[int] = [0, 0, 0, 0]):
+        ipx, ipx0 = numpy.zeros_like(self.x[0]), 0
+        for i in range(len(mom)):
+            ip = 2j * pi * mom[i] / self.latt_info.global_size[i]
+            ipx += ip * self.x[i]
+            ipx0 += ip * x0[i]
+        return LatticeComplex(self.latt_info, arrayExp(ipx - ipx0, getCUDABackend()))
+
+    def getPhases(self, mom_mode_list: Sequence[Sequence[int]], x0: Sequence[int] = [0, 0, 0, 0]):
+        phases = MultiLatticeComplex(self.latt_info, len(mom_mode_list))
+        for idx, mom_mode in enumerate(mom_mode_list):
+            phases[idx] = self.getPhase(mom_mode, x0)
+        return phases
+
+
+class GridPhase:
+    def __init__(self, latt_info: LatticeInfo, stride: Sequence[int]) -> None:
+        self.latt_info = latt_info
+        self.stride = stride
+        self.x = LocationPhase(latt_info).x
+
+    def getPhase(self, t_srce: Sequence[int]):
+        # sx, sy, sz, st = (x + gx * Lx) % Sx, (y + gy * Ly) % Sy, (z + gz * Lz) % Sz, (t + gt * Lt) % St
+        phase = numpy.ones(self.x[0].shape, "<i4")
+        for i in range(self.latt_info.Nd):
+            phase &= self.x[i] >= t_srce[i] and (self.x[i] - t_srce[i]) % self.stride[i] == 0
+        return LatticeInt(self.latt_info, phase)
+
+    def getPhases(self, t_srce_list: Sequence[Sequence[int]]):
+        phases = MultiLatticeInt(self.latt_info, len(t_srce_list))
+        for idx, t_srce in enumerate(t_srce_list):
+            phases[idx] = self.getPhase(t_srce)
+        return phases
